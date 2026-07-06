533463ee6c83e1b9b5b4e101da4f68334f02c1a4
MateusRodrigues-dev <contato.matt.rodrigues@gmail.com>
2026-07-06 20:42:54 -0300

    prog26.py

---
+++ prog26.py
@@ -0,0 +1,9 @@
+V1 = int(input("Digite um valor"))
+V2 = int(input("Digite um valor"))
+if V1>V2:
+    print(f"O valor {V1} é maior que {V2} ")
+elif V2>V1:
+    print(f"O valor {V2} é maior que {V1}")
+else:
+    print("Os valores são iguais")
+    
