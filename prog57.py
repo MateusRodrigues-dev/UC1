6a2e61dbdc8626cfbaaf7b9b112e88ba1ed65c2f
MateusRodrigues-dev <contato.matt.rodrigues@gmail.com>
2026-07-15 21:29:24 -0300

    commit dia 15/07

---
+++ prog57.py
@@ -0,0 +1,12 @@
+def verificar_idade(ano_nascimento):
+    ano_atual = 2026
+    idade = ano_atual - ano_nascimento
+    print(f"Idade :{idade} anos")
+    if idade < 18:
+        print("Menor de idade")
+    elif idade >= 65:
+        print("Idoso")
+    else:
+        print("Maior de idade")
+ano = int(input("Digite o ano de nascimento: "))
+verificar_idade(ano)
