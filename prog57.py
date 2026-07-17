53fc448b744e5368569c1a196cec423a72e8b413
MateusRodrigues-dev <contato.matt.rodrigues@gmail.com>
2026-07-16 22:52:59 -0300

    att

--- prog57.py
+++ prog57.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def verificar_idade(ano_nascimento):
     ano_atual = 2026
     idade = ano_atual - ano_nascimento
@@ -10,17 +9,4 @@ def verificar_idade(ano_nascimento):
     else:
         print("Maior de idade")
 ano = int(input("Digite o ano de nascimento: "))
-=======
-def verificar_idade(ano_nascimento):
-    ano_atual = 2026
-    idade = ano_atual - ano_nascimento
-    print(f"Idade :{idade} anos")
-    if idade < 18:
-        print("Menor de idade")
-    elif idade >= 65:
-        print("Idoso")
-    else:
-        print("Maior de idade")
-ano = int(input("Digite o ano de nascimento: "))
->>>>>>> dab6716 (teste commit pc casa)
 verificar_idade(ano)
--- prog58.py
+++ prog58.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def verificar_media (n1,n2,n3,n4):
     media =(n1 + n2 + n3 + n4)/4 
     print(F"\nMédia do aluno: {media:.2f}")
@@ -12,19 +11,4 @@ nota1 = float(input("Digite a 1º nota: "))
 nota2 = float(input("Digite a 2º nota: "))
 nota3 = float(input("Digite a 3º nota: "))
 nota4 = float(input("Digite a 4º nota: "))
-=======
-def verificar_media (n1,n2,n3,n4):
-    media =(n1 + n2 + n3 + n4)/4 
-    print(F"\nMédia do aluno: {media:.2f}")
-    if media >=7:
-        print("Situação: Aprovado")
-    elif media >=5:
-        print("Situação: Recuperação")
-    elif media <= 4:
-        print("Situação: Reprovado")
-nota1 = float(input("Digite a 1º nota: "))
-nota2 = float(input("Digite a 2º nota: "))
-nota3 = float(input("Digite a 3º nota: "))
-nota4 = float(input("Digite a 4º nota: "))
->>>>>>> dab6716 (teste commit pc casa)
 verificar_media(nota1,nota2,nota3,nota4)
